6d107fa0caccbdb882f5427252c3afd605a0b224
Erick Allan <e.allan@auramind.ai>
2026-01-18 20:22:46 -0300

    feat: enhance Reserveme app configuration
    - Add a docstring for the ReservemeConfig class to clarify its purpose
    - Implement a ready method to import signals when the app is initialized

--- reserveme/apps.py
+++ reserveme/apps.py
@@ -1,6 +1,13 @@
+"""Configuração do app reserveme."""
 from django.apps import AppConfig
 
 
 class ReservemeConfig(AppConfig):
+    """Configuração do app reserveme."""
+    
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reserveme'
+    
+    def ready(self):
+        """Importa signals quando app está pronto."""
+        import reserveme.signals  # noqa
